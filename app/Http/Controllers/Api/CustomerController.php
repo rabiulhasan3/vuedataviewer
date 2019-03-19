2f7ba32c7c085c948807e436d839302aaf80cd98
rabiulhasan3 <rabiul.fci@gmail.com>
2019-03-19 11:45:08 +0600

    Rest Api crud done

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -17,7 +17,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return new CustomerCollection( Customer::latest()->paginate(12) );
+        return new CustomerCollection( Customer::latest()->paginate(10) );
     }
 
     /**
@@ -28,7 +28,24 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name'    => 'required',
+            'email'   => 'required|email|unique:customers',
+            'phone'   => 'required|numeric',
+            'address' => 'required',
+            'total'   => 'required|numeric',
+        ]);
+
+        $customer          = new Customer();
+
+        $customer->name    = $request->name;
+        $customer->email   = $request->email;
+        $customer->phone   = $request->phone;
+        $customer->address = $request->address;
+        $customer->total   = $request->total;
+        $customer->save();
+
+        return new CustomerResource($customer);
     }
 
     /**
@@ -51,7 +68,24 @@ class CustomerController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'name'    => 'required',
+            'email'   => 'required|email|unique:customers,email,'.$id,
+            'phone'   => 'required|numeric',
+            'address' => 'required',
+            'total'   => 'required|numeric',
+        ]);
+
+        $customer          = Customer::findOrFail($id);
+
+        $customer->name    = $request->name;
+        $customer->email   = $request->email;
+        $customer->phone   = $request->phone;
+        $customer->address = $request->address;
+        $customer->total   = $request->total;
+        $customer->save();
+
+        return new CustomerResource($customer);
     }
 
     /**
@@ -62,6 +96,14 @@ class CustomerController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $customer =  Customer::findOrFail($id);
+        $customer->delete();
+        return new CustomerResource($customer);
     }
+
+    public function search($field,$query)
+    {
+        return new CustomerCollection(Customer::where($field,'LIKE',"%$query%")->latest()->paginate(10));
+    }
+
 }
